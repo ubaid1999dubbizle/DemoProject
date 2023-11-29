bbc5b8f4b56b8765c9fd4fe0d209ea51d6ce8337
Ubaid Ur Rehman <ubaidur.rehman@dubizzlelabs.com>
2023-11-29 19:29:11 +0500

    Initial Changes

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+
+//app. functionaltieis include get post use
+//let's say we create app. for '/admin' and latter text includes use
+// by just typing the url, we can directly access the admin portal. 
+//but if we use app.post(...'/admin'...) we can only access this page if we send a valid post request through a form or button 
+
+
+var corOptions = {
+    origin: 'https://localhost:3000'
+    
+}
+
+
+
+//middleware 
+app.use(cors(corOptions));
+
+
+//for api we are getting information in json format
+app.use(express.json());
+
+
+//used to parse and return middeware which looks at the request 
+app.use(express.urlencoded({extended: true}));
+
+
+
+//testing api
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello world'})
+})
+
+
+//Port 
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`server is running on port  ${PORT}`)
+})
